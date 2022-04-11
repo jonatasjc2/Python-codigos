19b881d1cf5afd8dbf7efa3e0abe1f4eb00ee05e
JonatasMello <jonatasjc2@gmail.com>
2022-04-11 17:36:04 -0300

    Atualizando o código de jogos.py e continuando o aperfeiçoamento do jogo da forca

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -2,7 +2,7 @@ import random
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
-    print("*********************************")
+    print("*********************************\n")
     
     
     arquivo = open("palavras.txt","r")
@@ -22,33 +22,43 @@ def jogar():
     enforcou = False
     acertou = False
     erros = 0
+    
+    print(letras_acertadas,"\n")
 
-    print(letras_acertadas)
-
-    while(not enforcou and not acertou):
+    while(not enforcou  and not acertou):
 
         chute = input("Qual letra? ")
         chute = chute.strip().upper()
-
-        if(chute in palavra_secreta):
-            index = 0
-            for letra in palavra_secreta:
-                if(chute == letra):
-                    letras_acertadas[index] = letra
-                index += 1
-        else:
-            erros += 1
-
+        
+        
+        if(chute==palavra_secreta):
+            
+            print("\nParabéns a palavra é esta:",palavra_secreta,".\nVocê ganhou!!")
+            break
+            
+        else:   
+            if(chute in palavra_secreta):
+                index = 0
+                for letra in palavra_secreta:
+                    if(chute == letra):
+                        letras_acertadas[index] = letra
+                    index += 1
+                    
+            else:
+                total_de_tentativas = 6
+                erros += 1
+                print("\nTentativa {} de {}\n".format(erros, total_de_tentativas))
+    
         enforcou = erros == 6
         acertou = "_" not in letras_acertadas
-        print(letras_acertadas)
+        print(letras_acertadas,"\n")
 
 
     if(acertou):
-        print("Você ganhou!!")
-    else:
-        print("Você perdeu!!")
-    print("Fim do jogo")
+        print("\nVocê ganhou!!")
+    if(enforcou):
+        print("\nVocê perdeu!!\nA palavra era {}.".format(palavra_secreta))
+    print("\nFim do jogo")
 
 if(__name__ == "__main__"):
     jogar()
--- Jogos/jogos.py
+++ Jogos/jogos.py
@@ -4,17 +4,17 @@ import adivinhacao
 def escolhe_jogo():
     print("*********************************")
     print("*******Escolha o seu jogo!*******")
-    print("*********************************")
+    print("*********************************\n")
 
-    print("(1) Forca (2) Adivinhação")
+    print("(1) Forca (2) Adivinhação\n")
 
     jogo = int(input("Qual jogo? "))
 
     if(jogo == 1):
-        print("Jogando forca")
+        print("\nJogando forca\n")
         forca.jogar()
     elif(jogo == 2):
-        print("Jogando adivinhação")
+        print("\nJogando adivinhação\n")
         adivinhacao.jogar()
 
 if(__name__ == "__main__"):
