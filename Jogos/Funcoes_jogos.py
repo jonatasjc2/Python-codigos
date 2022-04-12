678806f4be8af88f484e54f93e2b971a3e10b485
JonatasMello <jonatasjc2@gmail.com>
2022-04-12 15:47:42 -0300

    Adicionando uma função nova

--- Jogos/Funcoes_jogos.py
+++ Jogos/Funcoes_jogos.py
@@ -1,5 +1,31 @@
-def bem_vindo_a_forca():
+import random 
+def welcome_to_forca():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
     
+def loading_secret_word():
+    arquivo = open("palavras.txt","r")
+    frutas = []
+    
+    for linha in arquivo:
+        linha = linha.strip()
+        frutas.append(linha)
+     
+    arquivo.close()
+    
+    numero_aleatorio = random.randrange(0,len(frutas))
+    
+    palavra_secreta = frutas[numero_aleatorio].upper()
+    return palavra_secreta
+ 
+'''
+def 
+
+def
+
+def 
+
+def
+
+'''
