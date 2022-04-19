8e3b2372adf4e21327c6576f460007b25c351575
JonatasMello <jonatasjc2@gmail.com>
2022-04-19 11:45:12 -0300

    Adicionando um novo arquivo para enriquecer o jogo da forca, implementando palavras de paises

--- Jogos/Funcoes_jogos.py
+++ Jogos/Funcoes_jogos.py
@@ -2,69 +2,25 @@ import random
 import forca
 import adivinhacao 
 
-def the_chosen_game(game):
-    if(game == 1):
-        print("\nJogando forca\n")
-        forca.jogar()
-    elif(game == 2):
-        print("\nJogando adivinhação\n")
-        adivinhacao.jogar()
-        
-def welcome():
-    print("*********************************")
-    print("*******Escolha o seu jogo!*******")
-    print("*********************************\n")
+'''aqui começa as funções do jogo da forca'''
+
+def loading_file_paises():
+    arquivo = open("paises.txt","r")
+    paises = []
     
-def mensagem_final_do_jogo(acertou,numero_secreto):
-    if(acertou):
-        pass
-    else:
-        print("A número secreto era: {}\nFim do jogo".format(numero_secreto))
-
-def contagem_de_pontos(points,secret_number,guess_number): 
-    pontos_perdidos = abs(secret_number - guess_number)
-    points = points - pontos_perdidos
-    return points
-
-def mensagem_de_erro(maior,menor):
-    if(maior):
-        print("\nVocê errou! O seu chute foi maior do que o número secreto.\n")
-    elif(menor):
-        print("\nVocê errou! O seu chute foi menor do que o número secreto.\n")
-        
-def welcome_to_forca():
-    print("*********************************")
-    print("***Bem vindo ao jogo da Forca!***")
-    print("*********************************")
-
-def guess_number(): 
-    chute_str = input("Digite um número entre 1 e 100: ")
-    print("\nVocê digitou " , chute_str)
-    chute = int(chute_str)
-    return chute    
+    for linha in arquivo:
+        linha = linha.strip()
+        paises.append(linha)
+     
+    arquivo.close()
     
-def welcome_to_guess_game():
-    print("*********************************")
-    print("Bem vindo ao jogo de Adivinhação!")
-    print("*********************************\n")
-
-def dificulty_guess_game():    
-    tentativas = 0
-    print("Qual nível de dificuldade?\n")
-    print("(1) Fácil\n(2) Médio\n(3) Difícil")
-    options = (input("Defina o nível: "))
-    options = int(options)
-    if(options == 1):
-        tentativas = 20
-    elif(options == 2):
-        tentativas = 10
-    else:
-        tentativas = 5
-        
-    return tentativas
+    numero_aleatorio = random.randrange(0,len(paises))
     
-def loading_secret_word():
-    arquivo = open("palavras.txt","r")
+    secret_word = paises[numero_aleatorio].upper()
+    return secret_word
+
+def loading_file_frutas():
+    arquivo = open("frutas.txt","r")
     frutas = []
     
     for linha in arquivo:
@@ -77,6 +33,36 @@ def loading_secret_word():
     
     palavra_secreta = frutas[numero_aleatorio].upper()
     return palavra_secreta
+
+def welcome_to_forca():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+def pull_the_options():
+    print("\nEscolha o assunto que te interessa\n")
+    print("(1) Frutas\n(2) Paises\n")
+    options = (input("\nQual é a sua escolha: "))
+    options = int(options)
+    return options
+
+def loading_secret_word():  
+    options = pull_the_options()
+    
+    while (options >= 0):
+        
+        if(options == 1):
+            secret_word = loading_file_frutas()
+            break
+        
+        elif(options == 2):
+            secret_word = loading_file_paises()
+            break
+        else:
+            print("\nPreste atenção!Você deve escolher uma alternativa valida\n")
+            options = pull_the_options()
+            continue
+    return secret_word
 
 def guess():
     entrada_de_dado = input("\nQual letra ou palavra, se você quer arriscar um chute? ")
@@ -177,4 +163,65 @@ def mistake_message(erros):
     print(" |            ")
     print("_|___         ")
     print()
-     
+        
+'''Aqui começa as funções do jogos.py'''
+
+def the_chosen_game(game):
+    if(game == 1):
+        print("\nJogando forca\n")
+        forca.jogar()
+    elif(game == 2):
+        print("\nJogando adivinhação\n")
+        adivinhacao.jogar()
+        
+def welcome():
+    print("*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************\n")
+
+'''Aqui começa as funções do jogo adivinhação'''
+    
+def mensagem_final_do_jogo(acertou,numero_secreto):
+    if(acertou):
+        pass
+    else:
+        print("A número secreto era: {}\nFim do jogo".format(numero_secreto))
+
+def contagem_de_pontos(points,secret_number,guess_number): 
+    pontos_perdidos = abs(secret_number - guess_number)
+    points = points - pontos_perdidos
+    return points
+
+def mensagem_de_erro(maior,menor):
+    if(maior):
+        print("\nVocê errou! O seu chute foi maior do que o número secreto.\n")
+    elif(menor):
+        print("\nVocê errou! O seu chute foi menor do que o número secreto.\n")
+    
+def welcome_to_guess_game():
+    print("*********************************")
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("*********************************\n")
+
+def guess_number(): 
+    chute_str = input("Digite um número entre 1 e 100: ")
+    print("\nVocê digitou " , chute_str)
+    chute = int(chute_str)
+    return chute    
+    
+
+
+def dificulty_guess_game():    
+    tentativas = 0
+    print("Qual nível de dificuldade?\n")
+    print("(1) Fácil\n(2) Médio\n(3) Difícil")
+    options = (input("Defina o nível: "))
+    options = int(options)
+    if(options == 1):
+        tentativas = 20
+    elif(options == 2):
+        tentativas = 10
+    else:
+        tentativas = 5
+        
+    return tentativas   
